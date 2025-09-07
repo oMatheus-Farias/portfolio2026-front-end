2ff84efc35660aaf78e84e1f58d50944935a8886
Matheus <matheusfariasdasneves@gmail.com>
2025-09-07 13:04:45 -0300

    chore: add shadcn/ui initial config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
+import { FlatCompat } from '@eslint/eslintrc';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,15 +10,6 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  {
-    plugins: {
-      'simple-import-sort': simpleImportSort,
-    },
-    rules: {
-      'simple-import-sort/imports': 'error',
-      'simple-import-sort/exports': 'error',
-    },
-  },
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
