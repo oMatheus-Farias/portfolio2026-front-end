cd93fbbd66e87c780553e97023ce9b416767f384
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-09-09 22:56:06 -0300

    feat: add theme in local storage and add hover card component

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,19 +1,26 @@
 import { ReactNode } from 'react';
+import { HoverCard, HoverCardContent, HoverCardTrigger } from './ui/HoverCard';
 
 type TopButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 interface ITopButtonProps extends TopButtonProps {
   children: ReactNode;
+  hoverMessage?: string;
 }
 
-export function ActionButton({ children, type, ...props }: ITopButtonProps) {
+export function ActionButton({ children, type, hoverMessage, ...props }: ITopButtonProps) {
   return (
-    <button
-      type={type}
-      className="flex h-8 w-8 items-center justify-center rounded-[8px] border border-[#F0F0F0] bg-white shadow-md/10 hover:cursor-pointer hover:shadow-lg dark:border-[#2B2B2B] dark:bg-[#353535]"
-      {...props}
-    >
-      {children}
-    </button>
+    <HoverCard openDelay={300} closeDelay={100}>
+      <HoverCardTrigger asChild>
+        <button
+          type={type}
+          className="flex h-8 w-8 items-center justify-center rounded-[8px] border border-[#F0F0F0] bg-white shadow-md/10 hover:cursor-pointer hover:shadow-lg dark:border-[#2B2B2B] dark:bg-[#353535]"
+          {...props}
+        >
+          {children}
+        </button>
+      </HoverCardTrigger>
+      {hoverMessage && <HoverCardContent className="w-fit bg-white p-2.5 text-sm dark:bg-[#353535]">{hoverMessage}</HoverCardContent>}
+    </HoverCard>
   );
 }
--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -5,20 +5,42 @@ import Image from 'next/image';
 import moonIcon from '../assets/moonIcon.png';
 import sunIcon from '../assets/sunIcon.png';
 import { ActionButton } from './ActionButton';
+import { useEffect, useState } from 'react';
+import { STORAGE_KEYS } from '@/constants/storageKeys';
 
 export function ModeToggle() {
-  const { setTheme, theme } = useTheme();
+  const [themeSeted, setThemeSeted] = useState<string>('light');
+  const { setTheme } = useTheme();
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem(STORAGE_KEYS.THEME);
+    if (storedTheme) {
+      setThemeSeted(storedTheme);
+    }
+  }, []);
+
+  function handleSetTheme(theme: string) {
+    setThemeSeted(theme);
+    localStorage.setItem(STORAGE_KEYS.THEME, theme);
+  }
 
   return (
     <ActionButton
       type="button"
       aria-label="Trocar tema"
+      hoverMessage="Trocar tema"
       onClick={() => {
-        setTheme(theme === 'dark' ? 'light' : 'dark');
+        if (themeSeted === 'dark') {
+          handleSetTheme('light');
+          setTheme('light');
+        } else {
+          handleSetTheme('dark');
+          setTheme('dark');
+        }
       }}
     >
       <div className="relative h-4 w-4">
-        <Image src={theme === 'dark' ? sunIcon : moonIcon} sizes="16px" alt="Trocar tema" fill />
+        <Image src={themeSeted === 'dark' ? sunIcon : moonIcon} sizes="16px" alt="Trocar tema" fill />
       </div>
     </ActionButton>
   );
